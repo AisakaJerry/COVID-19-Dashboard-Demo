8a297c2d196d1ffd5c866d940d0d6f5fe599f27f
user833 <57163991+user833@users.noreply.github.com>
2021-04-06 19:55:18 -0400

    Add files via upload

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Takeout
+from .models import Takeout,DoctorVisit,Symptom,MedicineHistory,SurroundingSituation,Trip
 
 admin.site.register(Takeout)
+admin.site.register(DoctorVisit)
+admin.site.register(Symptom)
+admin.site.register(MedicineHistory)
+admin.site.register(SurroundingSituation)
+admin.site.register(Trip)
--- polls/forms.py
+++ polls/forms.py
@@ -1,13 +1,43 @@
 from django import forms
-from .models import Takeout
-from .models import State
+from .models import Takeout,DoctorVisit,Symptom,MedicineHistory,SurroundingSituation,Trip
+import datetime
+today=datetime.date.today()
+this_month=today.month
+this_year=today.year
 
 #it tells django which model to create the form from
-class TopicForm(forms.ModelForm):
+class TopicFormTakeout(forms.ModelForm):
+    date=forms.DateField(widget=forms.SelectDateWidget(years=range(2021,2025)))
     class Meta:
         model=Takeout
         fields=['restaurant_name','dishes','date']
 
-class SelectStateForm(forms.Form):
-    SELVALUE = State.SELVALUE
-    sel_value = forms.CharField(max_length=10, widget=forms.widgets.Select(choices=SELVALUE))
+class TopicFormDoctorVisit(forms.ModelForm):
+    date=forms.DateField(widget=forms.SelectDateWidget(years=range(2021,2025)))
+    class Meta:
+        model=DoctorVisit
+        fields=['doctor_name','date']
+
+class TopicFormSymptom(forms.ModelForm):
+    date=forms.DateField(widget=forms.SelectDateWidget(years=range(2021,2025)))
+    class Meta:
+        model=Symptom
+        fields=['body_temperature','cough_severity','date']
+
+class TopicFormMedicineHistory(forms.ModelForm):
+    start_date=forms.DateField(widget=forms.SelectDateWidget(years=range(2021,2025)))
+    class Meta:
+        model=MedicineHistory
+        fields=['medicine_name','start_date','lasting_day']
+
+class TopicFormSurroundingSituation(forms.ModelForm):
+    last_meet_date = forms.DateField(widget=forms.SelectDateWidget(years=range(2021,2025)))
+    class Meta:
+        model=SurroundingSituation
+        fields=['positive_people_name','last_meet_date']
+
+class TopicFormTrip(forms.ModelForm):
+    departure_date=forms.DateField(widget=forms.SelectDateWidget(years=range(2021,2025)))
+    class Meta:
+        model=Trip
+        fields=['location','departure_date','lasting_day']
